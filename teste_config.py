09409a57dd6529cfcc60b49d5d9209173dfef78e
Arthur Yoshio Ueta <165923087+ArtArsas@users.noreply.github.com>
2025-11-13 19:06:17 -0300

    Delete teste_config.py

--- teste_config.py
+++
@@ -1,4 +0,0 @@
-# Esta linha é um 'print'. É como o comando SELECT no SQL,
-# mas em vez de buscar dados em uma tabela, ele exibe
-# uma mensagem na tela do terminal. É nosso primeiro teste de lógica.
-print("Configuração do Mentor de Código OK!")
